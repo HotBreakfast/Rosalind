3675de46f3d12ad692069679f54d9f931facbe54
chn650 <chn650@users.noreply.github.com>
2014-10-16 19:05:44 +0200

    Update 1C_PatternMatching.py

--- Textbook_Track/1C_PatternMatching.py
+++ Textbook_Track/1C_PatternMatching.py
@@ -1,5 +1,5 @@
 #Python 2.7.5
-def StartingPositions(Pattern, Genome):
+def PatternMatching(Pattern, Genome):
     positions = []
     for i in range(0, len(Genome)-len(Pattern)):
         if Genome[i: i + len(Pattern)] == Pattern:
@@ -9,6 +9,6 @@ def StartingPositions(Pattern, Genome):
 
 pattern = raw_input("Enter Pattern: ")
 genome = raw_input("Enter Genome: ")       
-starting_positions = StartingPositions(pattern, genome)
+starting_positions = PatternMatching(pattern, genome)
 
 print "Starting positions: ", starting_positions
