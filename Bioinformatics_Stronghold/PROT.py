2d5bc8ef5e377e2df4f788223b50b2663b2fd7c7
chn650 <chn650@users.noreply.github.com>
2014-10-22 16:27:04 +0200

    Create PROT.py

---
+++ Bioinformatics_Stronghold/PROT.py
@@ -0,0 +1,57 @@
+#Python 2.7.5 
+CODON_TABLE = {
+    "A": ["GCU", "GCC", "GCA", "GCG"],
+    "C": ["UGU", "UGC"],
+    "D": ["GAU", "GAC"],
+    "E": ["GAA", "GAG"],
+    "F": ["UUU", "UUC"],
+    "G": ["GGU", "GGC", "GGA", "GGG"],
+    "I": ["AUU", "AUC", "AUA"],
+    "H": ["CAU", "CAC"],
+    "K": ["AAA", "AAG"],
+    "L": ["UUA", "UUG", "CUU", "CUC", "CUA", "CUG"],
+    "M": ["AUG",],
+    "N": ["AAU", "AAC"],
+    "P": ["CCU", "CCC", "CCA", "CCG"],
+    "Q": ["CAA", "CAG"],
+    "R": ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"],
+    "S": ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"],
+    "T": ["ACU", "ACC", "ACA", "ACG"],
+    "V": ["GUU", "GUC", "GUA", "GUG"],
+    "W": ["UGG",],
+    "Y": ["UAU", "UAC"],
+    "": ["UAA", "UAG", "UGA"],
+}
+
+
+def ProteinTranslation(Pattern):
+    Protein =[]
+    i=0
+    while i < len(Pattern):
+        Codon = Pattern[i:i+3]
+        for j in CODON_TABLE:
+            for k in CODON_TABLE[j]:
+                if k==Codon:
+                    Protein.append(j)
+        i=i+3
+        
+    return "".join(Protein)
+    
+
+
+file_name = raw_input("Enter the file name: ")
+try:
+    file_handle = open(file_name)
+except:
+    print "File cannot be opened: ", file_name
+    exit()
+
+lines = file_handle.readlines()
+
+pattern = lines[0].rstrip()
+
+translation = ProteinTranslation(pattern)
+
+output  = open("output.txt", "w")
+output.write(translation)
+output.close()
