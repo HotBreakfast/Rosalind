83431d8299987bc23fafe631d0646c6c0a6690cc
chn650 <chn650@users.noreply.github.com>
2014-10-25 22:30:06 +0200

    Create PRTM.py

---
+++ Bioinformatics_Stronghold/PRTM.py
@@ -0,0 +1,35 @@
+#Python 2.7.5 
+AA_MASS = {
+    "A": 71.03711,
+    "C": 103.00919,
+    "D": 115.02694,
+    "E": 129.04259,
+    "F": 147.06841,
+    "G": 57.02146,
+    "H": 137.05891,
+    "I": 113.08406,    
+    "K": 128.09496,
+    "L": 113.08406,
+    "M": 131.04049,
+    "N": 114.04293,
+    "P": 97.05276,
+    "Q": 128.05858,
+    "R": 156.10111,
+    "S": 87.03203,
+    "T": 101.04768,
+    "V": 99.06841,
+    "W": 186.07931,
+    "Y": 163.06333
+}
+
+def ProteinMass(Protein):
+    Mass=0
+    for i in Protein:
+        for j in AA_MASS:
+            if i==j:
+                Mass=Mass+AA_MASS[j]
+    return round(Mass,3)
+            
+
+protein= raw_input("Enter protein: ")
+print "Protein mass: ", ProteinMass(protein)
